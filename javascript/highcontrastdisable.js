f66d3616ded0759e78f180586ec6265ec6a43014
Connor <connor.reid98@btinternet.com>
2021-08-20 14:22:08 +0100

    Fixed small issues and added disable high contrast mode

---
+++ javascript/highcontrastdisable.js
@@ -0,0 +1,7 @@
+function highcontrastdisable(){
+  var e = document.getElementById('blackbackground');
+  var f = document.getElementById('whitetext');
+
+  e.id = "precontrast";
+  f.id = "contrasttext";
+}
