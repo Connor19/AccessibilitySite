739be42fdc492dd8c0c58b68cf1daf09bdeaedda
Connor <connor.reid98@btinternet.com>
2021-09-01 13:15:14 +0100

    Small tweaks

--- javascript/highcontrast.js
+++ javascript/highcontrast.js
@@ -1,7 +1,7 @@
-function highcontrast(){
-  var e = document.getElementById('precontrast');
+function highcontrast(){  //High Contrast Mode Fucntion//
+  var e = document.getElementById('precontrast');  //Finds the ID of the background and text in the interactive example div//
   var f = document.getElementById('contrasttext');
 
-  e.id = "blackbackground";
+  e.id = "blackbackground";  //Changes the ID of both to elements to invert colours to give off effect of high contrast//
   f.id = "whitetext";
 }
--- javascript/highcontrastdisable.js
+++ javascript/highcontrastdisable.js
@@ -1,7 +1,7 @@
-function highcontrastdisable(){
-  var e = document.getElementById('blackbackground');
+function highcontrastdisable(){    //Disable High Contrast Mode Fucntion//
+  var e = document.getElementById('blackbackground'); //Finds the ID of the background and text in the interactive example div//
   var f = document.getElementById('whitetext');
 
-  e.id = "precontrast";
+  e.id = "precontrast";   //Changes the ID of both to original values//
   f.id = "contrasttext";
 }
